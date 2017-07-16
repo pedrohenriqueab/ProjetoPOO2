20cce2e507510baaed37067b910198d449891f28
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-07-15 22:26:55 -0300

    Questao3

--- Funcionario.py
+++ Funcionario.py
@@ -1,15 +1,10 @@
-from Questao2.Pessoa import *
+from Questao3.Pessoa import *
 class Funcionario(Pessoa):
-    def __init__(self, nome, idade, genero, salario, matricula, aux, inss):
-        super(Pessoa, self).__init__(nome, idade, genero)
-        self.salario = salario
-        self.matricula = matricula
-        self.aux = aux
-        self.inss = inss
+    def __init__(self, nome, endereco, tel, codigoSetor, salarioBase, imposto)
+        super(Pessoa, self).__init__(nome, endereco, tel)
+        self.codigoSetor = codigoSetor
+        self.salarioBase = salarioBase
+        self.imposto = imposto
 
-    def calcularINSS(self):
-        self.aux = self.salario / 0.11
-        self.inss = self.salario - self.aux
-
-    def __str__(self):
-        return "Funcionario [Nome: %s, Idade: %d, Genero: %s, Salario: %f, Matricula: %d, INSS: %f]" % (self.nome, self.idade, self.genero, self.salario, self.matricula, self.inss)
+    def calcularSalarioTotal(self):
+        self.salarioTotal = (self.salarioBase - self.imposto)
