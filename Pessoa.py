c7929a6f7e4531451ba31fb3ff4bcece900e1116
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2017-07-15 22:24:26 -0300

    Questao2

--- Pessoa.py
+++ Pessoa.py
@@ -1,5 +1,8 @@
 class Pessoa():
-    def __init__(self, nome, endereco, tel):
+    def __init__(self, nome, idade, genero):
         self.nome = nome
-        self.endereco = endereco
-        self.tel = tel
+        self.idade = idade
+        self.genero = genero
+
+    def __str__(self):
+        return "Pessoa [Nome: %s, Idade: %d, Genero: %s]" % (self.nome, self.idade, self.genero)
